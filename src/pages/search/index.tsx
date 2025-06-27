67026042a4b15e5c735247cbe7c191454826400d
notURandomDev <1250901577@qq.com>
2025-06-28 00:10:17 +0800

    feat：添加了「搜索」界面的搜索参数传递
    完成搜索后，输入框的参数会传入搜索结果界面中

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,15 +1,31 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { Search } from "@taroify/core";
+import { View, Text } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import "./index.scss";
+import Taro from "@tarojs/taro";
+import { useSearch } from "./useSearch";
 
-export default function Search () {
+export default function SearchPage() {
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log("Page loaded.");
+  });
+
+  const { textInput, setTextInput } = useSearch();
+
+  const handleSearch = () => {
+    console.log(textInput);
+    Taro.navigateTo({ url: `/pages/search-result/index?q=${textInput}` });
+  };
 
   return (
-    <View className='search'>
-      <Text>Hello world!</Text>
+    <View className="search">
+      <Search
+        placeholder="搜索曲目"
+        value={textInput}
+        onChange={(e) => setTextInput(e.detail.value)}
+        onSearch={handleSearch}
+      />
+      <Text>搜索界面</Text>
     </View>
-  )
+  );
 }
