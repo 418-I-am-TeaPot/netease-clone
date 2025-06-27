ef6cf28490ea4b7160be6f7b522b028db2e6c039
notURandomDev <1250901577@qq.com>
2025-06-28 00:05:11 +0800

    feat：添加「我的」界面跳转逻辑
    点击头像之后，跳转至编辑个人信息界面

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,15 +1,22 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { View, Text } from "@tarojs/components";
+import Taro, { useLoad } from "@tarojs/taro";
+import "./index.scss";
+import { Avatar } from "@taroify/core";
+import NCMiniPlayer from "@/components/NCMiniPlayer";
 
-export default function Me () {
+export default function Me() {
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log("Page loaded.");
+  });
+
+  const editProfile = () => {
+    Taro.navigateTo({ url: "/pages/profile/index" });
+  };
 
   return (
-    <View className='me'>
-      <Text>Hello world!</Text>
+    <View className="me">
+      <Avatar onClick={editProfile}>P</Avatar>
+      <NCMiniPlayer />
     </View>
-  )
+  );
 }
