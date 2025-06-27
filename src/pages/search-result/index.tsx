9497673e6e468b9910ec3ac0805e06be56826c71
notURandomDev <1250901577@qq.com>
2025-06-28 00:11:20 +0800

    feat：添加了「搜索结果」界面中接收参数的逻辑

--- src/pages/search-result/index.tsx
+++ src/pages/search-result/index.tsx
@@ -1,15 +1,17 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { View, Text } from "@tarojs/components";
+import Taro, { useLoad } from "@tarojs/taro";
+import "./index.scss";
 
-export default function SearchResult () {
-  useLoad(() => {
-    console.log('Page loaded.')
-  })
+export default function SearchResult() {
+  useLoad((options: Record<string, string>) => {
+    if (!options.q) return;
+
+    Taro.setNavigationBarTitle({ title: options.q });
+  });
 
   return (
-    <View className='search-result'>
-      <Text>Hello world!</Text>
+    <View className="search-result">
+      <Text>搜索结果界面</Text>
     </View>
-  )
+  );
 }
