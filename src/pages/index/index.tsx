8a63cea0432d6212e2470b8a6cbfa664a894422d
WHS1145 <3117989952@qq.com>
2025-06-29 08:26:06 +0800

    单曲组件测试

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,9 +1,13 @@
-import { Search } from "@taroify/core";
+import { PullRefresh, Search } from "@taroify/core";
 import { View, Text } from "@tarojs/components";
 import { useLoad } from "@tarojs/taro";
 import "./index.scss";
 import Taro from "@tarojs/taro";
 import NCMiniPlayer from "@/components/NCMiniPlayer";
+import SongListVertical from "@/components/SongLIstVertical";
+import SongListHorizental from "@/components/SongLIstHorizental";
+import { useState } from "react";
+import { title } from "process";
 
 export default function Index() {
   useLoad(() => {
@@ -14,9 +18,63 @@ export default function Index() {
     Taro.navigateTo({ url: "/pages/search/index" });
   };
 
+  const items = [
+    {
+      imgUrl: "https://p1.music.126.net/FcsrgetFoSqZmxRjkBh6BA==/109951169522292175.jpg?param=200y200",
+      title: "Viyella's Memory",
+      artist: "123",
+      id: 123456
+    },
+    {
+      imgUrl: "../../assets/icons/tab/vault.png",
+      title: "87654321",
+      artist: "321",
+      id: 1234567
+    },
+    {
+      imgUrl: "../../assets/icons/tab/vault.png",
+      title: "87654321",
+      artist: "321",
+      id: 1234567
+    },
+    {
+      imgUrl: "../../assets/icons/tab/vault.png",
+      title: "87654321",
+      artist: "321",
+      id: 1234567
+    },
+    {
+      imgUrl: "../../assets/icons/tab/vault.png",
+      title: "87654321",
+      artist: "321",
+      id: 1234567
+    },
+    {
+      imgUrl: "https://p1.music.126.net/FcsrgetFoSqZmxRjkBh6BA==/109951169522292175.jpg?param=200y200",
+      title: "87654321",
+      artist: "321",
+      id: 1234567
+    }
+  ]
+
   return (
     <View className="index">
       <Search placeholder="搜索曲目" onClick={handleSearch} />
+
+      <View>
+        <SongListHorizental
+          items={items}
+        />
+      </View>
+
+      <View style="
+        width: 80%;
+        margin-left: 10%;">
+        <SongListVertical
+          items={items}
+          search={""}
+        />
+      </View>
       <NCMiniPlayer />
     </View>
   );
