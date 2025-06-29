197efda5a416b7fe20af0c6d1267cd80fa6c3741
WuJiaZhe123 <1094559361@qq.com>
2025-06-29 16:17:53 +0800

    feat:添加歌曲封面信息组件，实现收藏功能

---
+++ src/components/SongInfo/index.tsx
@@ -0,0 +1,49 @@
+import { Text, View } from "@tarojs/components";
+import { Image, Loading } from "@taroify/core";
+import { Like, LikeOutlined } from "@taroify/icons";
+import { usePlayerStore } from "@/store/player";
+
+import "./index.scss";
+
+export default function SongInfo() {
+  const { currentSong, isLike, setIsLike } = usePlayerStore();
+
+  const handleLike = () => {
+    setIsLike();
+  };
+
+  return (
+    <View className="songInfo container-v">
+      {/* 封面信息 */}
+      <View
+        style={{ justifyContent: "center", height: "80%" }}
+        className="songImg container-h grow"
+      >
+        <Image
+          width={600}
+          height={600}
+          className="border"
+          lazyLoad
+          src={currentSong?.coverUrl || "https://img.yzcdn.cn/vant/cat.jpeg"}
+          shape="circle"
+          placeholder={<Loading />}
+          fallback={
+            <text style={{ marginTop: "40%" }}>failed loading image</text>
+          }
+        />
+      </View>
+      {/* 歌曲信息与收藏功能 */}
+      <View className="songDetail  container-h grow">
+        <View className="container-v">
+          <Text>歌手：{currentSong?.name || "unknowed"}</Text>
+          <Text>歌名：{currentSong?.artists || "unknowed"}</Text>
+        </View>
+        {isLike ? (
+          <Like onClick={handleLike} size={20} />
+        ) : (
+          <LikeOutlined onClick={handleLike} size={20} />
+        )}
+      </View>
+    </View>
+  );
+}
